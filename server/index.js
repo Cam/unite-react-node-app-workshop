edd7bb59abbd9336a4003e9d7e73aa1c9c442fa6
Matt <matt.seccafien@shopify.com>
2018-05-03 16:50:42 -0400

    Step2

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 import dotenv from 'dotenv';
 import Koa from 'koa';
+import session from 'koa-session';
+import createShopifyAuth from '@shopify/koa-shopify-auth';
 
 dotenv.config();
+const {SHOPIFY_API_KEY, SHOPIFY_SECRET} = process.env;
 
 const app = new Koa();
+
+app.keys = [SHOPIFY_SECRET];
+
+app.use(session(app));
+
+app.use(
+  createShopifyAuth({
+    apiKey: SHOPIFY_API_KEY,
+    secret: SHOPIFY_SECRET,
+    scopes: ['write_orders, write_products'],
+    afterAuth(ctx) {
+      const {shop, accessToken} = ctx.session;
+
+      console.log('We did it!', accessToken);
+
+      ctx.redirect('/');
+    },
+  }),
+);
 
 app.use(function index(ctx) {
   ctx.body = 'Hello koa :)';
