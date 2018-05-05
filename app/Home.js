c0c938b097ee7c1e9c9eac17e5d3fcb1169e5c27
Matt <matt.seccafien@shopify.com>
2018-05-05 19:55:47 -0400

    step9

--- app/Home.js
+++ app/Home.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Board game loader</h1>
-      <p>
+import GameList from './GameList';
+
+const API_URL = 'https://bgg-json.azurewebsites.net/hot';
+
+export default class Home extends React.Component {
+  state = {
+    games: [],
+  };
+
+  componentDidMount() {
+    fetch(API_URL, {mode: 'cors'})
+      .then((response) => response.json())
+      .then((games) => this.setState({games}));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Board game loader</h1>
+        <GameList games={this.state.games} />
         <Link to="/settings">settings</Link>
-      </p>
-    </div>
-  );
+      </div>
+    );
+  }
 }
