9449aa421afaebef47c27eada83d4dc8165d5b82
Matt <matt.seccafien@shopify.com>
2018-05-05 07:27:36 -0400

    step7

--- app/App.js
+++ app/App.js
@@ -1,9 +1,18 @@
 import * as React from 'react';
+import {Switch, Route} from 'react-router';
+
+import Settings from './Settings';
+import NotFound from './NotFound';
+import Home from './Home';
 
 export default function() {
   return (
-    <div>
-      <h1>Board game loader</h1>
-    </div>
+    <React.Fragment>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/settings" component={Settings} />
+        <Route component={NotFound} />
+      </Switch>
+    </React.Fragment>
   );
 }
--- client/index.js
+++ client/index.js
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 import App from '../app/App';
 
-ReactDOM.render(<App>My cool app</App>, document.getElementById('app'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('app'),
+);
