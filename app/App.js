35e5fd85a872efbed072016699548e8ac0401211
Matt <matt.seccafien@shopify.com>
2018-05-05 19:29:17 -0400

    step8

--- app/App.js
+++ app/App.js
@@ -1,13 +1,17 @@
 import * as React from 'react';
-import {Switch, Route} from 'react-router';
+import {Switch, Route, withRouter} from 'react-router';
+import RoutePropagator from '@shopify/react-shopify-app-route-propagator';
 
 import Settings from './Settings';
 import NotFound from './NotFound';
 import Home from './Home';
 
+const Propagator = withRouter(RoutePropagator);
+
 export default function() {
   return (
     <React.Fragment>
+      <Propagator />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/settings" component={Settings} />
