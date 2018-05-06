21a7f15af9a6099857f8b91dc733f818aa7178da
Matt <matt.seccafien@shopify.com>
2018-05-05 21:38:26 -0400

    step10

--- app/Home.js
+++ app/Home.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import * as React from 'react';
 import {Link} from 'react-router-dom';
+import Fetch from 'react-fetch-component';
 
 import GameList from './GameList';
 
 const API_URL = 'https://bgg-json.azurewebsites.net/hot';
 
-export default class Home extends React.Component {
-  state = {
-    games: [],
-  };
+export default function Home() {
+  return (
+    <div>
+      <h1>Board game loader</h1>
+      <Fetch url={API_URL} as="json">
+        {({loading, error, data}) => {
+          if (loading) {
+            return <p>loading</p>;
+          }
+          if (error) {
+            return <p>failed to fetch games</p>;
+          }
 
-  componentDidMount() {
-    fetch(API_URL, {mode: 'cors'})
-      .then((response) => response.json())
-      .then((games) => this.setState({games}));
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Board game loader</h1>
-        <GameList games={this.state.games} />
-        <Link to="/settings">settings</Link>
-      </div>
-    );
-  }
+          return <GameList games={data} />;
+        }}
+      </Fetch>
+      <Link to="/settings">settings</Link>
+    </div>
+  );
 }
